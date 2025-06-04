1e4dc988e2eb8ae044e62e46519c6e011c15969d
Ashwini-Anand01 <anandrkt120@gmail.com>
2025-06-04 14:40:04 +0530

    Update If-Else(UC-1).js

--- IF-ELSE/If-Else(UC-1).js
+++ IF-ELSE/If-Else(UC-1).js
@@ -1,6 +1,6 @@
 const name="Ashwini Anand"; // person name is given which will be constant
 let  randomnumber=Math.floor(Math.random()*2); // random number will generated and  2 is written because only we 2 possible output either present or absent so 0,1 will give
-var absent=0; // here we give absent as 0 so that conditional operation when we perform we have already assign one value so that if other value will be present
+let absent=0; // here we give absent as 0 so that conditional operation when we perform we have already assign one value so that if other value will be present
 if(randomnumber==0){
     console.log("absent");
 }
